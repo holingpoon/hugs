f6fefe20ae224a3a94f57e6e75c4416946e845c9
Ho-Ling Poon <holingpoon@gmail.com>
2015-07-28 20:32:47 +0000

    Make Twig count!

--- src/Controller/HugsController.php
+++ src/Controller/HugsController.php
@@ -4,8 +4,20 @@ namespace Drupal\hugs\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-class HugsController extends ControllerBase {
-  public function hug ($to, $from) {
+class HugsController extends ControllerBase { 
+  public function hug($to, $from, $count) {
+    if (!$count) {
+      $count = $this->config('hugs.settings')->get('default_count');
+    }
+    return [
+      '#theme' => 'hug_page',
+      '#from' => $from,
+      '#to' => $to,
+      '#count' => $count
+    ];
+  }
+
+  public function hug2 ($to, $from) {
     return [
       '#theme' => 'hug_page',
       '#from' => $from,
